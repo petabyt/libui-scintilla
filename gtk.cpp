a7d9101002cd15dfc7343fd0a1933f92607bee95
Daniel C <brikbusters@gmail.com>
2025-04-25 16:47:52 -0400

    Initial from libui-dev

---
+++ gtk.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <ui.h>
+#include <gtk/gtk.h>
+#include <Scintilla.h>
+#include <ScintillaTypes.h>
+#include <ScintillaStructures.h>
+#include <ui_unix.h>
+#include <ScintillaWidget.h>
+#include "ui_scintilla.h"
+
+#define FOURCC(a, b, c, d) ((a) << 24 | ((b) << 16) | ((c) << 8) | (d))
+
+#define uiScintilla(this) ((uiScintilla *)this)
+#define uiScintillaSignature FOURCC('S', 'c', 'i', 'n')
+
+struct uiScintilla {
+	struct uiUnixControl c;
+	GtkWidget *widget;
+	ScintillaObject *sci;
+};
+
+uiUnixControlAllDefaults(uiScintilla)
+
+_UI_EXTERN uiScintilla *uiNewScintilla() {
+	struct uiScintilla *s;
+
+	uiUnixNewControl(uiScintilla, s);
+
+	s->widget = scintilla_new();
+	s->sci = SCINTILLA(s->widget);
+
+	return s;
+}
+
+uintptr_t uiScintillaSendMessage(uiScintilla *s, uint32_t code, uintptr_t w, uintptr_t l) {
+	return scintilla_object_send_message(s->sci, code, w, l);
+}
+
+void uiScintillaGetRange(uiScintilla *s, int start, int end, char *text) {
+	struct Scintilla::TextRange tr;
+	tr.chrg.cpMin = start;
+	tr.chrg.cpMax = end;
+	tr.lpstrText = reinterpret_cast<char *>(text);
+	scintilla_object_send_message(s->sci, SCI_GETTEXTRANGE, 0, reinterpret_cast<gintptr>(&tr));
+}
+
+void uiScintillaSetText(uiScintilla *s, const char *text) {
+	scintilla_object_send_message(s->sci, SCI_SETTEXT, strlen(text), reinterpret_cast<gintptr>(text));
+}
+
+void uiScintillaAppend(uiScintilla *s, const char *text) {
+	scintilla_object_send_message(s->sci, SCI_APPENDTEXT, strlen(text), reinterpret_cast<gintptr>(text));
+}
+
+int uiScintillaGetLength(uiScintilla *s) {
+	return scintilla_object_send_message(s->sci, SCI_GETLENGTH, 0, 0);
+}
+
+char *uiScintillaText(uiScintilla *s) {
+	int len = uiScintillaGetLength(s);
+	char *text = (char *)malloc((size_t)len);
+	uiScintillaGetRange(s, 0, len, text);
+	return text;
+}
