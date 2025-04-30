c28525e05340b967dbf82b9a8c21756c3fddfd06
Daniel C <brikbusters@gmail.com>
2025-04-29 22:25:43 -0400

    fix windows

--- win.cxx
+++ win.cxx
@@ -6,13 +6,11 @@
 #include <Scintilla.h>
 #include <ScintillaTypes.h>
 #include <ScintillaWin.h>
-
-#include "../../include/ui_windows.h"
+#include <ui_windows.h>
 extern HWND utilWindow;
 extern HINSTANCE hInstance;
 extern void uiprivDestroyTooltip(uiControl* c);
-
-#include "ui_scintilla.h"
+#include <ui_scintilla.h>
 
 #define uiScintilla(this) ((uiScintilla *)this)
 #define uiScintillaSignature 0x1234
@@ -42,25 +40,6 @@ _UI_EXTERN uiScintilla *uiNewScintilla() {
 	s->hwnd = CreateWindowEx(0,
 		"Scintilla", "Source", WS_CHILD | WS_VISIBLE | WS_TABSTOP,
 		0, 0, 100, 100, utilWindow, NULL, hInstance, NULL);
-
-//	const COLORREF red = RGB(0xFF, 0, 0);
-//	const COLORREF offWhite = RGB(0xFF, 0xFB, 0xF0);
-//	const COLORREF darkGreen = RGB(0, 0x80, 0);
-//	const COLORREF darkBlue = RGB(0, 0, 0x80);
-//	SetAStyle(SCE_H_DEFAULT, black, white, 11, "Times New Roman");
-
-	SendMessage(s->hwnd, SCI_STYLESETFONT, 0, reinterpret_cast<LPARAM>("Cascadia Mono"));
-
-	SendMessage(s->hwnd, SCI_STYLESETSIZE, 0, 10);
-
-	SendMessage(s->hwnd, SCI_STYLESETFORE, STYLE_DEFAULT, 0x121212);
-	SendMessage(s->hwnd, SCI_STYLESETBACK, STYLE_DEFAULT, 0x121212);
-	SendMessage(s->hwnd, SCI_SETELEMENTCOLOUR, SC_ELEMENT_CARET, 0xff0000ff);
-	SendMessage(s->hwnd, SCI_SETELEMENTCOLOUR, SC_ELEMENT_WHITE_SPACE, 0xff0000ff);
-	SendMessage(s->hwnd, SCI_SETELEMENTCOLOUR, SC_ELEMENT_WHITE_SPACE_BACK, 0xff0000ff);
-
-
-	SendMessage(s->hwnd, SCI_STYLESETSIZE, 0, 10); 
 
 	return s;
 }
